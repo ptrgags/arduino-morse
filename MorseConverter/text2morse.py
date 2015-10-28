a6580c8b0ded88189535f9334fe0fdfc17398b2e
Peter Gagliardi <ptrgags@live.com>
2015-10-27 20:35:54 -0400

    Refactor BinaryMessenger class
    Refactored the serial messaging functions into a class that handles
    sending data to Arduino from a variety of formats from bytes to
    strings of '0's and '1's

--- MorseConverter/text2morse.py
+++ MorseConverter/text2morse.py
@@ -1,25 +1,8 @@
-import serial
 import string
 import sys
+from BinaryMessenger import BinaryMessenger
 
-#optional padding at the end of the message
-PADDING = b'\x00'
-
-def clear(ser):
-    ser.write(b'\x00')
-
-#TODO: clean this up
-def write_message(ser, message, padding = True):
-
-    if padding:
-        message += PADDING
-
-    #Message prepended with length of message
-    message_length = ints_to_bytes([len(message)])
-    
-    ser.write(message_length + message)
-    
-
+#TODO: Support full morse code alphabet
 def text_to_subset(text):
     '''
     Convert a string to the subset of
@@ -35,6 +18,7 @@ def text_to_subset(text):
 
     return ''.join(x for x in text.upper() if x in SUBSET)
 
+#TODO: Support full morse code alphabet
 TO_MORSE = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -45,54 +29,36 @@ TO_MORSE = {
     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', 
     '0': '-----', ' ': ' '
 }
+
+#TODO: Move to converter library
 def text_to_morse_sequence(text):
     return [TO_MORSE.get(x, '') for x in text_to_subset(text)]
 
+#TODO: Move to converter library
 def text_to_morse_str(text):
     return " ".join(text_to_morse_sequence(text_to_subset(text)))
 
+#TODO: move to convert library
 TO_BINARY = {
     '.': "1",
     '-': "111",
     ' ': '0'
 }
+
+#TODO: Move to converter library
 def morse_seq_to_binary_str(morse):
     return '000'.join('0'.join([TO_BINARY.get(mark, []) for mark in character]) for character in morse)
-    
-def bin_str_to_ints(bin_str, bits_per_int=8):
-    counter = 0
-    output = []
-    current_int = 0
-    for bit in bin_str:
-        current_int = current_int << 1 | int(bit)
-        counter += 1
-        if counter == bits_per_int:
-            output.append(current_int)
-            current_int = 0
-            counter = 0
-    if current_int:
-        output.append(current_int << (bits_per_int - counter))
-    return output
-
-def ints_to_bytes(ints):
-    return bytes(bytearray(ints))
-    
-def text_to_morse_bytes(text):
-    return ints_to_bytes(bin_str_to_ints(morse_seq_to_binary_str(text_to_morse_sequence(text))))
     
 if __name__ == '__main__':
     port = sys.argv[1]
     
-    ser = serial.Serial(port, 9600, timeout=1)
-    
-    clear(ser)
+    messenger = BinaryMessenger(port)
+    messenger.clear_display()
     
     try:
         while True:
             in_data = raw_input("Enter a message >")
             print text_to_morse_str(in_data)
-            write_message(ser, text_to_morse_bytes(in_data))
+            messenger.send_bin_str(morse_seq_to_binary_str(text_to_morse_sequence(in_data)))
     except KeyboardInterrupt:
         print "Shutting down..."
-    finally:
-        ser.close()
